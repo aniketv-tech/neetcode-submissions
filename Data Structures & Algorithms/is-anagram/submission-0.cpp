a93011b176ab0dd8bf714fddacaadbc176e4d9f3
aniketv-tech <aniketvishwakarma1626@gmail.com>
2026-08-07 20:56:49 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+        unordered_map<char, int> freq;
+        
+        for(int i = 0; i < s.length(); i++)
+        {
+            freq[s[i]]++;
+        }
+        for(int i = 0; i < t.length(); i++)
+        {
+            freq[t[i]]--;
+        }
+        for (auto it : freq) {
+        if (it.second != 0){
+            return false;
+        }
+    
+
+    }
+    return true;
+    }
+};
